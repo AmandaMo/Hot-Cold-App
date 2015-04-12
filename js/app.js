f84f7eb1bd8d67f8df7d33496ff086831908f1d3
AmandaMo <amanda.j.morale@gmail.com>
2015-04-12 00:40:10 -0500

    hot cold gauge working

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@
 $(document).ready(function(){
 	
 	var chosenNumber = Math.floor((Math.random() * 100) + 1);
-	
-	var count = 0;
+	console.log(chosenNumber);
+	count = 0;
+
+	guess = 0;
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -17,12 +19,13 @@ $(document).ready(function(){
   	});
 
   	$("input.button").click(function(){
-  		var guess= $("#userGuess").val();
+  		guess= $("#userGuess").val();
   			if (guess>0 && guess%1===0 && guess<100){
   				$("ul.guessBox").append("<li>"+ guess +"</li>");
   				$("#userGuess").val("");
   				$("#count").text(count+1);
   				count = count +1;
+  				testNumber();
   			}
   			else {
   				window.alert("Please enter a valid integer between 1 and 100.");
@@ -30,7 +33,36 @@ $(document).ready(function(){
   			}
   	});
 
+  	function testNumber(){
+  		if (chosenNumber-guess>=50){
+  			$("#feedback").text("Ice Cold");
+  		}
+  		else if (chosenNumber-guess<50 && chosenNumber-guess>=30){
+  			$("#feedback").text("Cold");
+  		}
+  		else if (chosenNumber-guess>=20 && chosenNumber-guess<30){
+  			$("#feedback").text("warm");
+  		}
+  		else if (chosenNumber-guess<20 && chosenNumber-guess>=10){
+  			$("#feedback").text("hot");
+  		}
+  		else if (chosenNumber-guess<10 && chosenNumber-guess!==0){
+  			$("#feedback").text("very hot");
+  		}
+  		else{
+  			$("#feedback").text("YOU GOT IT! YAY!");
+  		}
+  	}
 
+  	function newGame(){
+  		var chosenNumber = Math.floor((Math.random() * 100) + 1);
+		console.log(chosenNumber);
+		count = 0;
+		guess = 0;
+		$("#count").text(count);
+		$("#userGuess").val("");
+		$("ul.guessBox" + li).remove();
+  	}
 
 });
 
